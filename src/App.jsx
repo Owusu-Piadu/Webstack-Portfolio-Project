838e598feb369c301c1447592b88bf596580b07c
Tarirohope <Priscilakapuyanyika974@gmail.com>
2024-10-27 12:55:31 +0100

    modified:   src/App.jsx 	modified:   src/components/Player.jsx 	new file:   src/context/PlayerContext.jsx 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Sidebar from './components/sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 const App = () => {
+
+  const {audioRef,track} = useContext(PlayerContext)
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
@@ -11,8 +15,12 @@ const App = () => {
       <Display/>
       </div>
       <Player/>
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
   )
 }
 
 export default App
+
+
+
