213fa4c3a0c90ca47745581a864affb3a0c3b719
Owusu-Piadu <eugeneowusupiadu@gmail.com>
2024-11-09 10:38:54 +0100

    new file:   package-lock.json 	new file:   package.json 	modified:   spotify-backend/src/controllers/songController.js

--- spotify-backend/src/controllers/songController.js
+++ spotify-backend/src/controllers/songController.js
@@ -1,25 +1,41 @@
-import {v2 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary'
 import songModel from '../models/songModel.js';
 
-const addSong = async (req,res) => {
+const addSong = async (req, res) => {
     try {
         const name = req.body.name;
         const desc = req.body.desc;
         const album = req.body.album;
         const audioFile = req.files.audio[0];
         const imageFile = req.files.image[0];
-        const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type: "video"} );
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image"} );
 
-        console.log (name, desc, album, audioUpload, imageUpload)
+        // Upload files to Cloudinary
+        const audioUpload = await cloudinary.uploader.upload(audioFile.path, { resource_type: "video" });
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
+        const duraion = `${Math.floor(audioUpload.duration / 60)}:${Math.floor(audioUpload.duration % 60)}`
+
+        // Save song to database
+        const songData = {
+            name,
+            desc,
+            album,
+            audio: audioUpload.secure_url,
+            image: imageUpload.secure_url,
+            duration,
+        };
+
+        const song = songModel(songData);
+        await song.save();
+
+        res.json({ success: true, message: "Song Uploaded Successfully" })
 
     } catch (error) {
-        
+        res.json({ success: false })
     }
 }
 
-const listSong = async (req,res) => {
-    
+const listSong = async (req, res) => {
+
 }
 
-export {addSong, listSong}
+export { addSong, listSong }
